834ae8e922453a99da910bd8828e34de54fb2dcf
tusharrao198 <tushar.rao198@gmail.com>
2021-02-28 18:33:31 +0530

    updated views

--- eduapp/views.py
+++ eduapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from eduapp.models import *
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'eduapp/home.html')
+    query = Quiz.objects.all()
+    print("QUERY",query)
+    query=query[0]
+    context = {
+        "name" : query.name,
+        "desc": query.description,
+        "time": query.time_created,
+    }
+    return render(request, 'eduapp/home.html', context)
