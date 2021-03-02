c5d249b469ade9ba3c1149373c40d36b6cd98bcf
tusharrao198 <tushar.rao198@gmail.com>
2021-03-02 18:23:07 +0530

    added secret key configuration for running locally

--- edumarker/settings.py
+++ edumarker/settings.py
@@ -21,14 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-try:
-    SECRET_KEY = config("SECRET_KEY")
-except:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+#
+# try:
+#     SECRET_KEY = config("SECRET_KEY")
+# except:
+#     SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # try and except for deployment purpose
-
 SECRET_KEY = "kgy4)xn&)a-@#o8yi%#y!x-+!by33y*z!ia^b%xiff2ai1^jjj"
 # Secretly for running directly to showcase project
 
